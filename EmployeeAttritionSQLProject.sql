b8d142a240d43f411db4e3e605ab3b836ecaae8e
MalcolmQSmith <121270158+MalcolmQSmith@users.noreply.github.com>
2023-06-20 20:21:57 -0400

    Add files via upload
    Updated file to include a stored procedure to analyze job roles with the highest attrition

--- EmployeeAttritionSQLProject.sql
+++ EmployeeAttritionSQLProject.sql
@@ -110,27 +110,36 @@ WHERE Attrition = 'Yes'
 
 ----Finding the attrition rate for each department
 
-SELECT Department, ROUND((COUNT(*) * 100) / (SELECT COUNT(*) FROM EmployeeAttrition WHERE Attrition = 'Yes'), 1) AS AttritionRate
+SELECT Department, ROUND((COUNT(*) * 100) / (SELECT COUNT(*) FROM EmployeeAttrition WHERE Attrition = 'Yes'),1) AS AttritionRate
 FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
 GROUP BY Department
 ORDER BY AttritionRate DESC
 
-
 --- Question 5: What is the attrition rate between the job roles?
+-----Find total attrition by job role
+SELECT JobRole, COUNT(*) AS JobRoleAttrition
+	FROM EmployeeAttrition
+	WHERE Attrition = 'Yes'
+	GROUP BY JobRole
+	ORDER BY JobRoleAttrition DESC
 
-SELECT TOP(5) JobRole, ROUND((COUNT(*) * 100) / (SELECT COUNT(*) FROM EmployeeAttrition WHERE Attrition = 'Yes'), 1) AS JobRoleAttritionRate
+SELECT JobRole, ROUND((COUNT(*) * 100) / (SELECT COUNT(*) FROM EmployeeAttrition WHERE Attrition = 'Yes'),1) AS JobRoleAttritionRate
 FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
 GROUP BY JobRole
 ORDER BY JobRoleAttritionRate DESC
 
 
+
 --- Question 6: What is the average tenure of those who left the company?
+
 
 SELECT AVG(TotalWorkingYears) AS AverageTenureAttrition
 FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
+
+
 
 --- Questions 7: What is the average monthly income of those who left the company?
 
@@ -159,11 +168,11 @@ FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
 
 --- Question 11: What is the average job satisfaction for those in the departments with the highest attrition?
-SELECT Department, AVG(JobSatisfaction) AS SatisfactionAvg
+SELECT Department, JobRole, AVG(JobSatisfaction) AS SatisfactionAvg
 FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
-GROUP BY Department
-ORDER BY SatisfactionRate DESC
+GROUP BY Department, JobRole
+ORDER BY SatisfactionAvg DESC
 
 --- Question 12: What is the average relationship satisfaction for those in the departments with the highest attrition?
 SELECT Department, AVG(RelationshipSatisfaction) AS RelationshipSatisfactionAvg
@@ -171,3 +180,57 @@ FROM EmployeeAttrition
 WHERE Attrition = 'Yes'
 GROUP BY Department
 ORDER BY RelationshipSatisfactionAvg DESC
+
+--- Deeper Analysis: Why did R&D (Research and Development) and Sales have the highest attrition?
+	
+	----- Creating a procedure to answer the following analysis:
+CREATE PROCEDURE AttritionfromResearchandSales
+AS
+	BEGIN
+		DECLARE @AttritionStatus VARCHAR(3); 
+		SET @AttritionStatus = 'Yes';
+
+		DECLARE @Department1 VARCHAR(50);
+		SET @Department1 = 'Research & Development';
+
+		DECLARE @Department2 VARCHAR(50);
+		SET @Department2 = 'Sales';
+
+		-- Question 1: What was the distribution of travel frequency for each department by job role?
+		SELECT JobRole, Department, BusinessTravel, COUNT(*) AS BusinessTravelAttritionbyJobRole
+		FROM EmployeeAttrition
+ 		WHERE Attrition = @AttritionStatus AND (Department = @Department1 OR Department = @Department2)
+		GROUP BY JobRole, Department, BusinessTravel
+		ORDER BY BusinessTravelAttritionbyJobRole DESC
+
+		-- Question 2: What was the distribution of relationship satisfaction for those in R&D and sales? 
+
+		SELECT JobRole, Department, RelationshipSatisfaction, COUNT(*) AS RelationshipAttritionCount
+		FROM EmployeeAttrition
+		WHERE Attrition = @AttritionStatus AND (Department = @Department1 OR Department = @Department2)
+		GROUP BY JobRole, Department, RelationshipSatisfaction
+		ORDER BY RelationshipAttritionCount DESC
+
+		--- Question 3: Performance Rating?
+
+		SELECT JobRole, Department, PerformanceRating, AVG(*) AS AvgPerformanceRating
+		FROM EmployeeAttrition
+		WHERE Attrition = @AttritionStatus AND (Department = @Department1 OR Department = @Department2)
+		GROUP BY JobRole, Department, PerformanceRating
+		ORDER BY AvgPerformanceRating DESC
+
+		--- Question 4: Work/life balance Average
+		SELECT JobRole, Department, WorkLifeBalance, AVG(*) AS AvgWorkLifeBalance
+		FROM EmployeeAttrition
+		WHERE Attrition = @AttritionStatus AND (Department = @Department1 OR Department = @Department2)
+		GROUP BY JobRole, Department, WorkLifeBalance
+		ORDER BY AvgWorkLifeBalance DESC
+
+	END;
+
+
+
+
+
+
+
